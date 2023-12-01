5bf6652fda7ab525f9e0acfe93a6a290b29ff269
Eric Christensen <chris4th@att.net>
2023-11-30 22:03:23 -0600

    push

---
+++ Mech Wars/Assets/Scripts/Guns/GunType.cs
@@ -0,0 +1,17 @@
+namespace LlamAcademy.Guns
+{
+    /// <summary>
+    /// Type of the gun. 
+    /// You should have one value for every gun you want to support in your game.
+    /// Default values provided are what are included in the demo.
+    /// </summary>
+    public enum GunType
+    {
+       DoubleCannon,
+       MiniGuns,
+       SmallGuns,
+       MissileHive1,
+       MissileHive2,
+       M4A1,
+    }
+}
